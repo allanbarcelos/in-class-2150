7107f7dbfd66d40828d3e0fccbf02a5762c59167
Allan Barcelos <allan@barcelos.dev>
2025-01-31 13:21:12 -0500

    improve the API

--- api/index.js
+++ api/index.js
@@ -5,17 +5,30 @@ const app = express();
 const port = 3000;
 
 const dbConfig = {
-    host: "",
+    host: "localhost",
     user: "root",
     password: "123456",
     database: "in-class-db",
+    port: 3310
 };
 
 // JSON
+/*
 
-app.use(express.json());
+{
+    id: 1
+    name: "Allan"
+}
 
-app.post("/api/register", async (req, res) => {
+*/
+
+app.use(express.json()); // express.json() => middleware 
+
+
+// POST, GET, PUT, PATCH, DELETE 
+// "/api/register" endpoint > http://localhost/api/register
+app.post("/api/register", async (req, res) => { /// every time we want create something
+
     const { name, email, password } = req.body;
 
     if(!name || !email || !password){
@@ -25,9 +38,8 @@ app.post("/api/register", async (req, res) => {
     try {
         const conn = await mysql.createConnection(dbConfig);
 
-        const query = "INSERT INTO userts (name, email, password) VALUES (?, ?, ?)";
+        const query = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
         await conn.execute(query, [name, email, password]);
-
         await conn.end();
         res.status(201).json({message: "Created with success"});
 
@@ -37,16 +49,23 @@ app.post("/api/register", async (req, res) => {
 });
 
 
+//
 app.get("/api/register", async (req, res) => {
     try {
         const conn = await mysql.createConnection(dbConfig);
         const [rows] = await conn.execute("SELECT * FROM users");
         await conn.end();
         res.status(200).json(rows); // array of objects with our data
-
     } catch (e) {
         res.status(500).json({ error: "Fail"});
     }
+});
+
+//
+app.get("/", async (req, res) => {
+
+        res.status(200).json({message: "API is running"}); // array of objects with our data
+
 });
 
 
@@ -59,13 +78,13 @@ async function initDatabase() {
 
         if(tables.length === 0){
             const createTableQuery = `
-                CREATE TABLE users {
+                CREATE TABLE users (
                     id INT AUTO_INCREMENT PRIMARY KEY,
                     name VARCHAR(200) NOT NULL,
                     email VARCHAR(200) NOT NULL UNIQUE,
                     password VARCHAR(100) NOT NULL,
-                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                };
+                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                );
             `;
 
             await conn.query(createTableQuery);
@@ -75,7 +94,7 @@ async function initDatabase() {
         await conn.end();
 
     } catch (error) {
-        console.log("Database error");
+        console.error("Database error ", error);
         process.exit(1);
     }
 }
@@ -85,3 +104,5 @@ initDatabase().then(() => {
         console.log(`The server is running, PORT: ${port}`)
     })
 })
+
+// promises: 2 states in promises ... reject(),  resolve()
